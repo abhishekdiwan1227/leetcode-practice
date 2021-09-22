7f5b062650b4717c969e649e1c2e0f4cdaaf686e
abhishekdiwan1227 <abhishekdiwan1227@gmail.com>
2021-09-22 21:58:22 +0530

    https://leetcode.com/problems/regular-expression-matching/

---
+++ 10_Regular_Expression_Matching.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        
+    }
+};
+
+int main() {
+    string s = "aab";
+    string p = "c*a*b";
+
+    // string s = "mississippi";
+    // string p = "mis*is*p*.";
+    // -> false
+
+    Solution sol = Solution();
+    cout << sol.isMatch(s, p);
+    return 0;
+}
+
