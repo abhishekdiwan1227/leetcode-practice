4f6716ab95cf6b5859e7e6ba4bb05c937ed0e089
abhishekdiwan1227 <abhishekdiwan1227@gmail.com>
2021-09-22 21:51:58 +0530

    https://leetcode.com/problems/zigzag-conversion/

--- 6_Zigzag_Conversion.cpp
+++ 6_Zigzag_Conversion.cpp
@@ -1,5 +1,41 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
-int main() {
+
+class Solution
+{
+public:
+    string convert(string s, int numRows)
+    {
+        if (numRows == 1)
+            return s;
+
+        vector<string> res = vector<string>(numRows, "");
+        int i = 0;
+        bool reverse = true;
+        for (char c : s)
+        {
+            res[i] = res[i] + c;
+            if (i == 0 || i == numRows - 1)
+                reverse = !reverse;
+            i = reverse ? --i : ++i;
+        }
+
+        string result = "";
+        for (string s : res)
+        {
+            result = result + s;
+        }
+
+        return result;
+    }
+};
+
+int main()
+{
+    string s = "PAYPALISHIRING";
+    int numRows = 3;
+    auto sol = new Solution();
+    cout << sol->convert(s, numRows);
     return 0;
 }
